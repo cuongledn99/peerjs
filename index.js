4c124764891c2a8bd1faadcc5a06f8930cc10230
Huy Cuong <huycuongdn1999@gmail.com>
2020-03-31 15:58:21 +0700

    demo 1

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ var privateKey  = fs.readFileSync('./certificates/key.pem', 'utf8');
 var certificate = fs.readFileSync('./certificates/cert.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
+console.log(credentials)
+
+var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 var LANAccess = "0.0.0.0";
+
+
+httpServer.listen(8080, LANAccess);
+httpsServer.listen(3000,LANAccess)
 
 // Serve the index.html file as content of the / route
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/index2.html'));
 });
 app.use('/js', express.static('./js'));
-app.listen(4004,LANAccess)
